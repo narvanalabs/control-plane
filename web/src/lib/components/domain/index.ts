0a8629f46cb39a8e5310cea65975bb4ca663eae1
Daniel Togey <dannykosgei@gmail.com>
2025-12-28 19:15:11 +0300

    Add ServiceCard export to domain components index

--- web/src/lib/components/domain/index.ts
+++ web/src/lib/components/domain/index.ts
@@ -3,3 +3,4 @@
  * These components are specific to the Narvana platform domain model
  */
 export { default as AppCard } from './AppCard.svelte';
+export { default as ServiceCard } from './ServiceCard.svelte';
